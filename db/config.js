5b2d1b90ea25698141ba9b12a9a186f92ae6e52a
Abdullah Aslam <aabdullahaslam91@gmail.com>
2025-07-28 13:02:51 -0700

    ENV CHANGES

--- db/config.js
+++ db/config.js
@@ -1,8 +1,14 @@
 const mongoose = require('mongoose')
 
 
-mongoose.connect('mongodb+srv://abdullahaslam:abdullah123@cluster0.w3oh5mb.mongodb.net/')
+// mongoose.connect('mongodb+srv://abdullahaslam:abdullah123@cluster0.w3oh5mb.mongodb.net/')
+// mongoose.connect('mongodb+srv://abdullahaslam:abdullah123@cluster0.w3oh5mb.mongodb.net/')
 
-
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => console.log('✅ MongoDB connected successfully'))
+    .catch((err) => console.error('❌ MongoDB connection error:', err));
 
 module.exports = mongoose
